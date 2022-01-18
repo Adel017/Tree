1db6ef267f12db03a9f402f380009c3be88b4194
Adel Toubal <>
2022-01-18 18:25:20 +0100

    First commit

---
+++ src/Exceptions/TreeException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Alba\Tree\Exceptions;
+
+use RuntimeException;
+
+class TreeException extends RuntimeException
+{
+    //
+}
---
+++ src/ServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Alba;
+
+use Alba\Tree\Console\Tree\FixTree;
+use Alba\Tree\Console\Tree\GrowTree;
+use Alba\Tree\Console\Tree\ShowTree;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\ServiceProvider;
+
+class SmoothieServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->commands([
+            FixTree::class,
+            GrowTree::class,
+            ShowTree::class,
+        ]);
+
+        Builder::macro('debugSql', function() {
+            $bindings = array_map(function ($value) {
+                return is_string($value) ? '"'.addcslashes($value, '"').'"' : $value;
+            }, $this->getBindings());
+            return vsprintf(str_replace('?', '%s', $this->toSql()), $bindings);
+        });
+
+        EloquentBuilder::macro('debugSql', function() {
+            $bindings = array_map(function ($value) {
+                return is_string($value) ? '"'.addcslashes($value, '"').'"' : $value;
+            }, $this->getBindings());
+            return vsprintf(str_replace('?', '%s', $this->toSql()), $bindings);
+        });
+
+        EloquentBuilder::macro('findInOrder', function ($ids, $columns = ['*']) {
+            return $this->findMany($ids, $columns)->sortByKeys($ids);
+        });
+
+        Collection::macro('sortByKeys', function(array $ids) {
+            $ids = array_flip(array_values($ids));
+            $i = $this->count();
+            return $this->sortBy(function ($model) use ($ids, &$i) {
+                return $ids[$model->getKey()] ?? ++$i;
+            });
+        });
+    }
+}
